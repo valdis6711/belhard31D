ad086e765f532a01d8a31407efb46cbac5b4e4f3
valdis6711 <malashkov.2014@mail.ru>
2022-05-16 20:38:45 +0300

    initial commit

--- 3_1.py
+++
@@ -1,7 +0,0 @@
-n = int(input("Введите количество элементов списка: "))
-a = []
-for i in range(0, n):
-    elem = int(input("Введите элемент списка: "))
-    a.append(elem)
-age = sum(a) / n
-print("Среднее значение элементов списка",round(age, 3))
---
+++ 5_2.py
@@ -0,0 +1,16 @@
+num_1 = int(input('введи 1-е число: '))
+num_2 = int(input('введи 2-е число: '))
+num_3 = int(input('введи 3-е число: '))
+num_4 = int(input('введи 4-е число: '))
+num_5 = int(input('введи 5-е число: '))
+print(' {} + {} / {} - {} * {} = '.format(num_1, num_2, num_3, num_4, num_5))
+print(num_1 + num_2 / num_3 - num_4 * num_5)
+
+print(' {} * {} - {} / {} + {} = '.format(num_1, num_2, num_3, num_4, num_5))
+print(num_1 * num_2 - num_3 / num_4 + num_5)
+
+print(' {} / {} * {} + {} - {} = '.format(num_1, num_2, num_3, num_4, num_5))
+print(num_1 / num_2 * num_3 + num_4 - num_5)
+
+print(' {} - {} + {} * {} / {} = '.format(num_1, num_2, num_3, num_4, num_5))
+print(num_1 - num_2 + num_3 * num_4 / num_5)

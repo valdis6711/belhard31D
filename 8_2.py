95654e96f2a48fb5550dc2831df8cc74f47abece
valdis6711 <malashkov.2014@mail.ru>
2022-05-26 15:12:50 +0300

    initial commit

---
+++ 8_2.py
@@ -0,0 +1,11 @@
+mylist = ["Шла Саша по шоссе и употребляла хлебобулочные изделия"]
+print(list(map(lambda x: x.lower(), mylist)))
+print(list(map(lambda x: x.upper(), mylist)))
+
+mylist = ["Хочешь мира - готовься к войне"]
+print(list(map(lambda x: x.lower(), mylist)))
+print(list(map(lambda x: x.upper(), mylist)))
+
+mylist = [" Не лезь вперёд батьки у пекло"]
+print(list(map(lambda x: x.lower(), mylist)))
+print(list(map(lambda x: x.upper(), mylist)))

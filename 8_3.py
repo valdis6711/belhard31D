b7bcadddc594a6daa950074246f07f2023f0e023
valdis6711 <malashkov.2014@mail.ru>
2022-05-26 15:43:20 +0300

    initial commit

---
+++ 8_3.py
@@ -0,0 +1,21 @@
+my_str= "Шла Саша по шоссе, употребляя хлебобулочные изделия!"
+my_str = my_str.replace(".", "")
+my_str = my_str.replace(",", "")
+my_str = my_str.replace("!", "")
+my_str = my_str.replace("-", "")
+print(my_str)
+
+my_str= "Хочешь мира - готовься к войне"
+my_str = my_str.replace(".", "")
+my_str = my_str.replace(",", "")
+my_str = my_str.replace("!", "")
+my_str = my_str.replace("-", "")
+print(my_str)
+
+my_str= "Не лезь вперед батьки у пекло"
+my_str = my_str.replace(".", "")
+my_str = my_str.replace(",", "")
+my_str = my_str.replace("!", "")
+my_str = my_str.replace("-", "")
+my_str = my_str.replace("в", " ")
+print(my_str)
